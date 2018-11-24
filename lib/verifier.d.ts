4da6a6f6fb219b8fdc4cd9ab456278e9d8062d66
Norbert Gørke <norgor@gmail.com>
2018-11-24 16:21:32 +0100

    Fixed null/undefined checking.

--- lib/verifier.d.ts
+++ lib/verifier.d.ts
@@ -1,10 +1,18 @@
 export declare class EmailSyntaxError extends Error {
     constructor(error: string);
 }
+/**
+ * Verifies that a string is a valid local part of an email address.
+ * @param local The local part to verify.
+ */
 export declare function verifyLocalPart(local: string): void;
+/**
+ * Verifies that a string is a valid domain of an email address.
+ * @param local The domain to verify.
+ */
 export declare function verifyDomain(domain: string): void;
 /**
- * Checks if the string is valid.
- * @param email The email address to validate.
+ * Verifies that a string is a valid email address.
+ * @param email The email to verify.
  */
 export declare function verifyEmail(email: string): void;
